967efb61cbf8dfa47b708769c898a35759e83200
Saurav-Ranjan <saurav.ranjan0523@gmail.com>
2023-07-31 22:44:11 +0530

    changes in header , footer and landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import {ModulesModule} from '../app/modules/modules.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModulesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
